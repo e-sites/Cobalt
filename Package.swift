5c9693b3a1a453fedfaef984ae6f1e72019dca81
Bas van Kuijck <bas@e-sites.nl>
2019-09-18 16:07:34 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
         .library(name: "Cobalt", targets: ["Cobalt", "CobaltTests"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "4.8.2")),
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/google/promises", .upToNextMajor(from: "1.2.8")),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", .upToNextMajor(from: "3.2.0")),
-        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.0.1"))
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/google/promises", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
     ],
     targets: [
         .target(
